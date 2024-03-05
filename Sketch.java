cd4492d215a777687359cea6942dd8490d9c6276
LianAustin <austin.lian25@ycdsbk12.ca>
2024-03-05 12:56:50 -0500

    Completed processing task 3

--- Sketch.java
+++ Sketch.java
@@ -1,46 +1,63 @@
 import processing.core.PApplet;
-import processing.core.PApplet;
 
 public class Sketch extends PApplet {
  /*
-  * A program that uses java processing to create a remote control
+  * A program that uses java processing to create a remote control which will randomly be placed on the screen as well as display the time
   * @author: Austin L
   */
 
   // Sets the size of the window
   public void settings() {
-    size(300, 300);
-
+    size(200, 200);
   }
 
-  /**
-   * Sets background to light green
-   */
+  // Sets the background color to green
   public void setup() {
-    background(210, 255, 173);
+    if(second() % 2 == 0) {
+      background(0,0,255);
+    } else if (second() % 2 != 0) {
+      background(0,255,0);
+    }
   }
 
-  /**
-   * Called repeatedly, anything drawn to the screen goes here
-   */
+  // Position the control at a random location
+  float fltControlX = random(width - 30);
+  float fltControlY = random(height - 30);
+
+  // Position the antenna at the center top of the control
+  float fltAntennaX = fltControlX + 7.5f; // Position the antenna at the middle of the control
+  float fltAntennaY = fltControlY - 15; // Position the antenna just above the control
+
+  // Position the button in the middle of the control
+  float fltButtonX = fltControlX + 15;
+  float fltButtonY = fltControlY + 15;
+
   public void draw() {
-    // Converts width and height to float
-    float fltWidth = (float) (width);
-    float fltHeight = (float) (height);
+    setup();
 
-	  // Creates rectangle (remote control antenna)
-    stroke(255);
-    rect((fltWidth / 2.033898305f), (fltHeight / 2.666666667f), (fltWidth / 20f), (fltHeight / 3f));
+    // Fetches the time from the computer and stores it in a string
+    int intSec = second();
+    int intMin = minute();
+    int intHour = hour();
+    String strTime = str(intHour) + ":" + str(intMin) + ":" + str(intSec);
 
-    // Creates rectangle (remote control)
+    // Displays the time on the screen
+    fill(50);
+    textSize(32);
+    textAlign(CENTER, CENTER);  // Set the text alignment to center
+    text(strTime, width / 2, height / 2);
+
+    // Creates rectangle (remote control antenna)
     stroke(255);
-    square((fltWidth / 3f), (fltHeight / 1.714285714f), fltWidth / 2.72727272727272727272727272f);
-    fill(0, 0, 0);
+    rect(fltAntennaX, fltAntennaY, 15, 15);
+
+    // Creates square (remote control)
+    stroke(255);
+    square(fltControlX, fltControlY, 30);
 
     // Creates circle (remote control button)
     stroke(255);
     fill(255, 0, 0);
-    ellipse((fltWidth / 1.935483871f) , (fltHeight / 1.304347826f), (fltWidth / 4f), (fltHeight / 4f));
-
+    ellipse(fltButtonX, fltButtonY, 10, 10);
   }
 }
